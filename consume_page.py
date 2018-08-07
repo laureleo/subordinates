3009c219717607051999ed9f70a37dd67be46a0f
sigis92 <hugringr@gmail.com>
2018-08-07 22:58:07 +0200

    the requested html does not match the inspected :/

--- consume_page.py
+++ consume_page.py
@@ -1,4 +1,4 @@
-#Given an url, downloads a webpage perfectly.
+#Downloads a link and the content linked to from that page
 import os
 import bs4
 import requests
@@ -10,15 +10,29 @@ from requests.compat import urljoin
 """
 def consume(url):
     print("Time to eat {}".format(url))
- #   response = get_response(url)
- #   clone_content(response)
- #   urls = extract_urls(response)
- #   for url in urls:
- #       get_response(url)
-
-    print(os.getcwd())
-    os.makedirs("./" + url.split('/')[-1])
+    mkdir(url)
+    response = get_response(url)
+    clone_content(response)
+    urls = extract_urls(response)
+    for url in urls:
+        response = get_response(url)
+        clone_content(response)
     print("It was delicious")
+
+
+"""
+@funct: creates a folder to contain all the content consumed and moves the execution process into that folder
+@param: an url
+"""
+def mkdir(url):
+    directory = "./" + url.split('/')[-1]
+    try:
+        os.makedirs(directory)
+        print("Directory created")
+    except Exception as exc:
+        print("Notification: %s" %(exc))
+
+    os.chdir(directory)
 
 
 """ 
@@ -27,26 +41,34 @@ def consume(url):
 @retur: a response object
 """
 def get_response(url):
-    response = requests.get(url)
     try:
-        response.raise_for_status()
+        response = requests.get(url)
+        return response
     except Exception as exc:
-        print("ERROR: %s" %(exc))
+        print("Notification: %s" %(exc))
 
-    return response
+
 
 """
 @funct: stores a local copy of the content found in the response object
 @param: a response object
 """
 def clone_content(response):
-    filename = response.url.split('/')[-1] 
-    if 'text/html' in response.headers['content-type']:
-        filename = filename + '.html'
-    copy = open(filename, 'wb+')
-    for chunk in response.iter_content(1024):
-        copy.write(chunk)
-    copy.close()
+    if response:
+        url = response.url
+        if url[-1] == '/':
+            url = url[:-1]
+        filename = url.split('/')[-1] 
+        if 'text/html' in response.headers['content-type']:
+            filename = filename + '.html'
+        try:
+            copy = open(filename, 'wb+')
+            for chunk in response.iter_content(1024):
+                copy.write(chunk)
+            copy.close()
+            print("downloaded " + filename)
+        except Exception as exc:
+            print("Notification: %s" %(exc))
 
 
 
@@ -57,21 +79,21 @@ def clone_content(response):
 """
 def extract_urls(response):
     urls = []
-    if 'text/html' in response.headers['content-type']:
-        soup = bs4.BeautifulSoup(response.content, 'html5lib')
-        links = soup.findAll('a')
-        for link in links:
-            link = link.get('href')
-            link = urljoin(response.url, link) # Makes relative urls absolute
-            urls.append(link)
-        #In case of duplicates, remove them
-        urls = set(urls)
+    if response:
+        if 'text/html' in response.headers['content-type']:
+            soup = bs4.BeautifulSoup(response.content, 'html5lib')
+            links = soup.findAll('a')
+            for link in links:
+                link = link.get('href')
+                link = urljoin(response.url, link) # Makes relative urls absolute
+                urls.append(link)
+            #In case of duplicates, remove them
+            urls = set(urls)
+            print("Found {} links".format(len(urls)))
     return urls
 
 
 
-#consume('https://imgur.com/gallery/U2DdZ4y')
-consume('https://www.youtube.com/watch?v=9bZkp7q19f0')
+#consume('https://www.youtube.com/watch?v=9bZkp7q19f0')
 #consume('http://www-personal.umich.edu/~csev/books/py4inf/media/Py4Inf-01-Intro.pdf')
-#consume('http://www-personal.umich.edu/~csev/books/py4inf/media')
-
+consume('https://www.facebook.com/groups/330486193693264')
