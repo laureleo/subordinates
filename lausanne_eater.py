fad388705abd4578fcad323a77ded4d77e2e3236
sigis92 <hugringr@gmail.com>
2018-08-10 01:27:13 +0200

    dealing with pages rendered with js

--- lausanne_eater.py
+++ lausanne_eater.py
@@ -4,32 +4,48 @@ import os
 import bs4
 import re
 import requests
+import datetime as dt
 from requests.compat import urljoin
-
+from selenium import webdriver
+#On using selenium http://stanford.edu/~mgorkove/cgi-bin/rpython_tutorials/Scraping_a_Webpage_Rendered_by_Javascript_Using_Python.php
 """
 @funct: takes an url, stores a copy of the site locally by downloading its html and a copy of all of the resources it links to.
 @param: an url
 """
 def consume(url):
-    print("Lausanne Eater ACTIVATED")
-    mkdir(url)
-    response = get_response(url)
-    urls = regex_links(response)
+    print("Creating a directory to store the results in")
+    make_directory(url)
 
-    url = urls[0]
-    print("CHECKING {}".format(url))
-    regex_price(response)
+    print("Loading mother page")
+    response = get_response_object(url)
 
-    extract_content(response)
+    print("Extracting urls to all objects for sale")
+    links = regex_links(response)
 
-    print("It was delicious")
+    for i, link in enumerate(links):
+        print("Loading article {}. Extracting content...".format(i))
+        url = link
+
+        html = get_js_content(url)
+
+        content = extract_content(html)
+        print("PRICE: {}\nPLACE: {}\nTIME: {}\n".format(content[0],content[1],content[2]))
+
+
+def get_js_content(url):
+    browser = webdriver.Chrome()
+    browser.get(url)
+    outer_html = browser.page_source
+    inner_html = browser.execute_script("return document.body.innerHTML")
+    return(inner_html)
 
 def regex_links(response):
     urls = []
     if response:
-        cleaned = response.text
-        pattern = re.compile("\/groups\/\d*?\/permalink\/.*?(?=sale_post)", re.IGNORECASE)
-        matches = re.findall(pattern, cleaned)
+        html = response.text
+        link_pattern = "\/groups\/\d*?\/permalink\/\d*?\/\?(?=sale_post)"
+        regex = re.compile(link_pattern, re.IGNORECASE)
+        matches = re.findall(regex, html)
         for match in matches:
             match = "https://www.facebook.com" + match
             urls.append(match)
@@ -37,22 +53,52 @@ def regex_links(response):
         print("extracted {} links from the page".format(len(urls)))
     return urls
 
-def regex_price(response):
-    price = 0
-    if response:
-        cleaned = response.text
-        print(response.text)
+def extract_content(html):
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+
+    price = soup.find('div',
+            attrs = {'class': '_l57'})
+    if not price:
+        price = 0
+    else:
+        price = calculate_price(price)
         
+    location = soup.find('div',
+            attrs = {'class': '_l58'})
+    if not location:
+        location = 'N/A'
+    else:
+        location = location.text
 
-def extract_content(response):
-    if response:
-        soup = bs4.BeautifulSoup(response.content, 'html5lib')
-        print("Page title is:\n{}".format(soup.title.string))
-        #a = soup.find_all('div', class_= "_3ccb")
-        a = soup.find_all('div', attrs = {
-            'class': '_lm7'})
+    description = soup.find('div',
+            attrs = {'class': '_5pbx userContent _3576'})
+    if not description:
+        description = 'N/A'
+    else:
+        description = description.text
 
-# beginning navigation:
+    time = soup.find('span',
+            attrs = {'class': 'timestampContent'})
+    if not time:
+        time = "N/A"
+    else:
+        time = calculate_time(time)
+
+    return(price, location, time, description)
+
+def calculate_price(price):
+    text = price.text
+    numbers = re.search('\d+.\d*', text).group(0)
+    cleaned = numbers.replace(",", "")
+    price = int(float(cleaned) * 9.06)
+    return price
+
+def calculate_time(time):
+    text = time.text
+    
+    return text
+
+
 
 
 
@@ -60,7 +106,7 @@ def extract_content(response):
 @funct: creates a folder to contain all the content consumed and moves the execution process into that folder
 @param: an url
 """
-def mkdir(url):
+def make_directory(url):
     directory = "./" + url.split('/')[-1]
     try:
         os.makedirs(directory)
@@ -72,11 +118,11 @@ def mkdir(url):
 
 
 """ 
-@funct: creates a local copy of the content existing at the specified url
+@funct: creates a response object while dealing with errors
 @param: an URL
 @retur: a response object
 """
-def get_response(url):
+def get_response_object(url):
     try:
         response = requests.get(url)
         return response
@@ -108,26 +154,8 @@ def clone_content(response):
 
 
 
-"""
-@funct: checks if the object is html/text. If it is, it returns all urls found in it
-@param: a response object
-@retur: a list of urls
-"""
-def extract_urls(response):
-    urls = []
-    if response:
-        if 'text/html' in response.headers['content-type']:
-            soup = bs4.BeautifulSoup(response.content, 'html5lib')
-            links = soup.findAll('a')
-            for link in links:
-                link = link.get('href')
-                link = urljoin(response.url, link) # Makes relative urls absolute
-                urls.append(link)
-            #In case of duplicates, remove them
-            urls = set(urls)
-            print("Found {} links".format(len(urls)))
-    return urls
 
 
 
-consume('https://www.facebook.com/groups/330486193693264')
+#consume('https://www.facebook.com/groups/330486193693264/permalink/2093037470771452/?sale_post_id=2093037470771452')
+consume("https://www.facebook.com/groups/330486193693264/forsaleposts/?story_id=2097041673704365")
