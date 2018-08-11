e9533224a310512cff26df9396c71f2865f15d2e
sigis92 <hugringr@gmail.com>
2018-08-11 11:34:40 +0200

    Decently functional

--- lausanne_eater.py
+++ lausanne_eater.py
@@ -6,31 +6,40 @@ import re
 import requests
 import json
 import datetime as dt
+import glob
+import time
 from requests.compat import urljoin
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 #On using selenium http://stanford.edu/~mgorkove/cgi-bin/rpython_tutorials/Scraping_a_Webpage_Rendered_by_Javascript_Using_Python.php
+#TODO: Read existing json into memory. 
 """
 @funct: takes an url, stores a copy of the site locally by downloading its html and a copy of all of the resources it links to.
 @param: an url
 """
 def update(url):
+    print("You are running update. This will create a new json object of all articles found on the website.")
+    print("You should probably make it so previously visited urls aren't re-visited sometime. Maybe")
     print("Creating a directory to store the results in")
     make_directory(url)
 
     print("Loading mother page")
-    response = get_response_object(url)
+    start_page = get_js_content_headless(url, True)
 
     print("Extracting urls to all objects for sale")
-    links = regex_links(response)
+    links = regex_links(start_page)
 
     articles = []
     for i, link in enumerate(links):
         print("Loading article {}. Extracting content...".format(i+1))
-        html = get_js_content_headless(link)
-        #html = get_js_content(link)
-        article = make_article(html, link)
-        articles.append(article)
+        try:
+            html = get_js_content_headless(link, False)
+            article = make_article(html, link)
+            articles.append(article)
+        except Exception as exc:
+            print("Notification: Skipping this article  %s" %(exc))
+            print(link)
 
     print("Storing all articles...")
     filename = str(dt.datetime.now())
@@ -52,14 +61,33 @@ def make_article(html, url):
     return article
 
 
-def get_js_content_headless(url):
+def get_js_content_headless(url, scroll):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
 
-    browser = webdriver.Chrome(chrome_options=options)
-    browser.get(url)
-    outer_html = browser.page_source
-    inner_html = browser.execute_script("return document.body.innerHTML")
+    try:
+        browser = webdriver.Chrome(chrome_options=options)
+        browser.get(url)
+        if scroll is True: 
+            lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+            match=False
+            count = 0
+            while(match==False):
+                count = count + 1
+                print("Scrolled to the bottom {} times...".format(count))
+                lastCount = lenOfPage
+                time.sleep(0.5)
+                lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+                if lastCount==lenOfPage:
+                    match=True
+
+        outer_html = browser.page_source
+        inner_html = browser.execute_script("return document.body.innerHTML")
+        browser.close()
+
+    except TimeoutException as ex:
+        print("Exception has been thrown. " + str(ex))
+        browser.close()
     return inner_html
 
 def get_js_content(url):
@@ -69,18 +97,16 @@ def get_js_content(url):
     inner_html = browser.execute_script("return document.body.innerHTML")
     return(inner_html)
 
-def regex_links(response):
+def regex_links(html):
     urls = []
-    if response:
-        html = response.text
-        link_pattern = "\/groups\/\d*?\/permalink\/\d*?\/\?(?=sale_post)"
-        regex = re.compile(link_pattern, re.IGNORECASE)
-        matches = re.findall(regex, html)
-        for match in matches:
-            match = "https://www.facebook.com" + match
-            urls.append(match)
-        urls = list(set(urls))
-        print("extracted {} links from the page".format(len(urls)))
+    link_pattern = "\/groups\/\d*?\/permalink\/\d*?\/\?(?=sale_post)"
+    regex = re.compile(link_pattern, re.IGNORECASE)
+    matches = re.findall(regex, html)
+    for match in matches:
+        match = "https://www.facebook.com" + match
+        urls.append(match)
+    urls = list(set(urls))
+    print("extracted {} links from the page".format(len(urls)))
     return urls
 
 def extract_content(html):
@@ -126,24 +152,37 @@ def extract_content(html):
 def calculate_price(price):
     text = price.text
     numbers = re.sub("\D", "", text) 
-    price = int(float(numbers) * 9.06)
+    try:
+        price = int(float(numbers) * 9.06)
+    except Exception as exc:
+        print("Notification: %s" %(exc))
+        print("Did not manage to deal with {} converted to {}".format(text, numbers))
+        price = 0
+
+
     return price
 
 def calculate_time(time):
     text = time.get('title')
     # this was the case when running witout the headless at lest match = re.search('(?P<dd>\d+)\/(?P<mm>\d+)\/(?P<yyyy>\d+)\s(?P<hh>\d+):(?P<m2>\d+)', text)
     match = re.search('(?P<yyyy>\d+)-(?P<mm>\d+)-(?P<dd>\d+)\s(?P<hh>\d+):(?P<m2>\d+)', text)
-    dd = int(match.group('dd'))
-    mm = int(match.group('mm'))
-    yyyy = int(match.group('yyyy'))
-    hh = int(match.group('hh'))
-    m2 = int(match.group('m2'))
+    try:
+        dd = int(match.group('dd'))
+        mm = int(match.group('mm'))
+        yyyy = int(match.group('yyyy'))
+        hh = int(match.group('hh'))
+        m2 = int(match.group('m2'))
 
-    date_article = dt.datetime(yyyy, mm, dd, hh, m2)
-    date_today = dt.datetime.today()
-    diff = date_today-date_article
-    time_elapsed = diff.days
+        date_article = dt.datetime(yyyy, mm, dd, hh, m2)
+        date_today = dt.datetime.today()
+        diff = date_today-date_article
+        time_elapsed = diff.days
     
+    except Exception as exc:
+        print("Notification: %s" %(exc))
+        print("Something went wrong with the time conversion. The string that failed was {}".format(text))
+        print("Setting time to 0")
+        time_elapsed = 0
     return time_elapsed
 
 
@@ -197,9 +236,53 @@ def clone_content(response):
         except Exception as exc:
             print("Notification: %s" %(exc))
 
+def run(url, command, p_range = [0, 50000], t_range = [0, 1000]):
+    print("Loading the latest saved copy of articles found in the folder...")
+    articles = load_latest(url)
+    print("{} articles found".format(len(articles)))
+    print("Showing all results with a cost between {} and {} SEK published between {} and {} days ago".format(p_range[0], p_range[1], t_range[0], t_range[1]))
+    if command is 'price':
+        print("Sorting on price:")
+        sorted_list = sorted(articles, key = lambda article: article['price'])
+    if command is 'latest':
+        print("Sorting on date:")
+        sorted_list = sorted(articles, key = lambda article: article['time'])
+
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    for article in sorted_list:
+        try:
+            if article['price'] <= p_range[1] and article['price'] >= p_range[0]:
+                if article['time'] <= t_range[1] and article['time'] >= t_range[0]:
+                    print_info(article)
+
+        except Exception as exc:
+            print("Notification: %s" %(exc))
+            print("Could not display results from {}".format(article['url']))
+
+def print_info(article):
+    print("\n{}".format(article['title']))
+    print("\tPrice = {} SEK".format(article['price']))
+    print("\t{} Days since the article was posted".format(article['time']))
+    print("\tLocation: {}".format(article['place']))
+    print("\t{}".format(article['url']))
 
 
+def load_latest(url):
+    directory = "./" + url.split('/')[-1]
+    os.chdir(directory)
+    list_of_files = glob.glob('*') # * means all if need specific format then *.csv
+    latest_file = max(list_of_files, key=os.path.getctime)
+    with open(latest_file, "r") as read_file:
+            data = json.load(read_file)
+    return data
 
 
+#update("https://www.facebook.com/groups/330486193693264/forsaleposts/?story_id=2097041673704365")
 
-update("https://www.facebook.com/groups/330486193693264/forsaleposts/?story_id=2097041673704365")
+
+sortby = 'price'
+pricerange = [0, 8000]
+timerange = [0, 14]
+
+run("https://www.facebook.com/groups/330486193693264/forsaleposts/?story_id=2097041673704365", sortby, pricerange, timerange)
+
